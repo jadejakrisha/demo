092cd2104dbe0e3365fe87289f9fa583f503f5b5
krisha_jadeja <krisha_jadeja@epam.com>
2025-03-06 10:21:43 +0530

    commit

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
 
     const response = {
         statusCode: 200,
-        message : 'Hello from Lambda',
+        message: 'Hello from Lambda',
     };
     return response;
 };
