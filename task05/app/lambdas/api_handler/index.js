a8a1fec55fa7dd6e7610f3bf7ff4226e512785ab
krisha_jadeja <krisha_jadeja@epam.com>
2025-03-08 16:23:22 +0530

    changed index

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -50,12 +50,12 @@ export const handler = async (event) => {
         console.log("DynamoDB Response:", response);
 
         const responseObject = {
-                    statusCode: 201,
-                    event: eventItem
-
-                };
-
-
+        statusCode: 201,
+        body: JSON.stringify(
+        {
+         statusCode: 201,
+           event: eventItem
+             })};
         return responseObject;
 
     } catch (error) {
